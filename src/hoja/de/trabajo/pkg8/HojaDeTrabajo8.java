d569d693739274563de80c33affa2a83aaeffbd0
Temp <Temp@VAIO>
2014-09-04 07:54:25 -0600

    new Commit

---
+++ src/hoja/de/trabajo/pkg8/HojaDeTrabajo8.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja.de.trabajo.pkg8;
+
+/**
+ *
+ * @author Temp
+ */
+public class HojaDeTrabajo8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
