987911a9f9588dd7fe119e6613106c55dbaadf4b
JoseEduardoCruz <josee10195@hotmail.com>
2014-09-09 20:00:48 -0500

    Update Nodo.java

--- src/hoja/de/trabajo/pkg8/Nodo.java
+++ src/hoja/de/trabajo/pkg8/Nodo.java
@@ -9,5 +9,31 @@ package hoja.de.trabajo.pkg8;
  * @author Temp
  */
 public class Nodo {
+    int frecuencia;     
+    char caracter;           
+
+    //Asigan una frecuencia a un nodo en particular
+    public Nodo(int frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    //construye un nodo con el caracter y frecuencia 1
+    public Nodo(char caracter) {
+        this.caracter = caracter;
+        this.frecuencia = 1;
+    }
     
+    @Override
+    public boolean equals(Object nodo) {
+        if (nodo == null) {
+            return false;
+        }
+        
+        Nodo other = (Nodo) nodo;
+        if (this.caracter != other.caracter) {
+            return false;
+        }
+        return true;
+    }
+
 }
